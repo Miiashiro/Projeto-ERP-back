fd8b120895de1631520b88b30189da84714f815c
Miiashiro <miiashiro.caio1@gmail.com>
2024-05-20 21:42:26 -0300

    Método de mostrar contas a pagar

--- src/Controllers/ControllerBill/billController.js
+++ src/Controllers/ControllerBill/billController.js
@@ -3,6 +3,21 @@ import db from "../../Services/Bill/billService.js"
 
 const routes = Express.Router()
 
+routes.get('/', async(req, res) => {
+    try{
+        const results = await db.showBill()
+
+        if (results.length == 0) {
+            res.status(204).send("Esperando entrada de dados")
+        } else {
+            res.status(200).json(results)
+        }
+        
+    } catch(err){
+        res.status(500).send({ message: `Erro ao tentar buscar os dados` })
+    }
+})
+
 routes.post('/', async(req, res) => {
     try{
         const { bill, price, date } = req.body
--- src/Services/Bill/billService.js
+++ src/Services/Bill/billService.js
@@ -1,5 +1,15 @@
 import database from "../../Repository/connection.js"
 
+async function showBill(){
+    const sql = "SELECT * FROM tbl_bill"
+    
+    const conn = await database.connect()
+    const [rows] = await conn.query(sql)
+    conn.end()
+
+    return rows
+} 
+
 async function createBill(bill, price, date){
     const sql = "INSERT INTO tbl_bill(name_bill, price, data_vencimento) VALUES(?, ?, ?)"
     const data = [bill, price, date]
@@ -9,4 +19,4 @@ async function createBill(bill, price, date){
     conn.end()
 } 
 
-export default {createBill}
+export default {createBill, showBill}
